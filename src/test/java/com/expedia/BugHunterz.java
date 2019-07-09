d1af5219bdc29a0fa41280ca13ac815520e085da
david <dghovakimyan@gmail.com>
2019-07-08 22:21:45 -0500

    Hi Kosta, i'm done!

--- src/test/java/com/expedia/BugHunterz.java
+++ src/test/java/com/expedia/BugHunterz.java
@@ -157,5 +157,53 @@ public class BugHunterz {
     }
 
 
+    @Test(priority = 1)
+    public void David_ChangeingTheLanguage_Test() throws InterruptedException {
+        WebElement language = driver.findElement(By.xpath("(//a[@id='onPageModalPickerLink01'])[2]"));
+        language.click();
+        Thread.sleep(4000);
+        WebElement languageButton = driver.findElement(By.xpath("(//div[@class='picker-dropdown menu-bar'])[7]"));
+        languageButton.click();
+        Thread.sleep(1000);
+        WebElement english = driver.findElement(By.xpath("(//*[@data-language-name='English'])[3]"));
+        english.click();
+        Thread.sleep(1000);
+        WebElement saveButton = driver.findElement(By.id("multi-currency-multi-lang-save-btn-header"));
+        saveButton.click();
+        driver.close();
+    }
+
+
+    @Test(priority = 2)
+    public void David_RentingACarByModel_Test() throws InterruptedException {
+        WebElement carButton = driver.findElement(By.id("tab-car-tab-hp"));
+        carButton.click();
+        Thread.sleep(3000);
+        WebElement pickuplocation = driver.findElement(By.id("car-pickup-hp-car"));
+        pickuplocation.sendKeys("Chicago, IL");
+        Thread.sleep(1000);
+        WebElement dropofflocation = driver.findElement(By.id("car-dropoff-hp-car"));
+        dropofflocation.sendKeys("Los Angeles, CA");
+        Thread.sleep(3000);
+        WebElement startDate = driver.findElement(By.id("car-pickup-date-hp-car"));
+        startDate.sendKeys("07/15/2019" + Keys.ENTER);
+        Thread.sleep(1000);
+    }
+
+
+    @Test(priority = 3)
+    public void David_LookAHotelByGuestRating_Test() throws InterruptedException {
+        WebElement hotelsButton = driver.findElement(By.id("tab-hotel-tab-hp"));
+        hotelsButton.click();
+        WebElement hoteldestination = driver.findElement(By.id("hotel-destination-hp-hotel"));
+        hoteldestination.sendKeys("Chicago, Illinois");
+        WebElement searchButton = driver.findElement(By.xpath("(//button[@data-gcw-change-submit-text='Search'])[1]"));
+        searchButton.click();
+        Thread.sleep(2000);
+        WebElement GuestRatingRadioButton = driver.findElement(By.id("radio-guestRating-45"));
+        GuestRatingRadioButton.click();
+
+    }
+
 
 }
